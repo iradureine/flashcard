0d4de0fba1ca3cab99c336798421bb3f19b2753e
iradureine <miradukunda601@gmail.com>
2021-11-23 21:18:38 -0800

    card

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -1,4 +1,4 @@
-import NewTopicForm from "../../components/NewTopicForm";
+//import NewTopicForm from "../../components/NewTopicForm";
 import { Link, useParams } from "react-router-dom";
 import ROUTES from "../../app/routes";
 import { useSelector } from "react-redux";
